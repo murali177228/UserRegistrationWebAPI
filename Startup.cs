c2fc483ddfe1d2254cd495317150a317d244c799
murali177228 <murali177228@gmail.com>
2020-12-12 17:07:36 +0200

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WebAPI.Models;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<AuthenticationContext>(Options =>
+                                Options.UseSqlServer(Configuration.GetConnectionString("IdentityConnections")));
+
+            services.AddDefaultIdentity<ApplicationUser>()
+                    .AddEntityFrameworkStores<AuthenticationContext>();
+
+            //To change the default validations of IdentityManager
+
+            services.Configure<IdentityOptions>(Options => {
+
+                Options.Password.RequireDigit = false;
+                Options.Password.RequireUppercase = false;
+                
+            }            
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
